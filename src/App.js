ebfd3ca3e99bd0becabd7f448ab49c596270675e
pratik <pratikgaikwad551@gmail.com>
2021-05-07 18:05:04 +0530

    project assignment update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    
-     <p>I am learning React. My life is getting better.</p>
+    <div>
+      <h1 data-ns-test="project-name">Pizza Delivery App</h1>
+      <p data-ns-test="project-name">The Web Application which will help people order online pizza. The user can Sign up and login
+      through the app, select the pizza they want, add to cart and make online payment. Also the pizza shop can see the orders which were booked by the people making this application for multiuser (customers as well as shop person).  
+      </p>
+    </div>
     
   );
 }
